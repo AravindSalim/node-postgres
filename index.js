37ce5ab4cd73ea4a3e7336ea984340f9aa1d9adf
aravindstarai <aravind@starailab.com>
2023-07-26 13:47:35 +0100

    changed header origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const feedback_model = require('./feedback_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
