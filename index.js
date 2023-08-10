4b4b299f4aea29e965f82a2146d8b30466992c66
aravindstarai <aravind@starailab.com>
2023-08-10 12:51:39 +0100

    question added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3003
 
 const feedback_model = require('./feedback_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://dzk22yqu.projects.cmp.uea.ac.uk');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
